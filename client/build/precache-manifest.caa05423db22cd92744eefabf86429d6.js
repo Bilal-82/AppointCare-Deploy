3d7dce2521acc282aa0a9db1a8bcf5c9e67c592b
Bilal-82 <123393537+Bilal-82@users.noreply.github.com>
2023-06-30 11:10:35 +0530

    Final Code

---
+++ client/build/precache-manifest.caa05423db22cd92744eefabf86429d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b3d3978385b92813f02",
+    "url": "/static/css/main.4850bae8.chunk.css"
+  },
+  {
+    "revision": "1b3d3978385b92813f02",
+    "url": "/static/js/main.1b3d3978.chunk.js"
+  },
+  {
+    "revision": "090dbcf4a522cc8e7c4d",
+    "url": "/static/js/1.090dbcf4.chunk.js"
+  },
+  {
+    "revision": "845b4bc84f2108d15fca",
+    "url": "/static/css/2.4dbf5d34.chunk.css"
+  },
+  {
+    "revision": "845b4bc84f2108d15fca",
+    "url": "/static/js/2.845b4bc8.chunk.js"
+  },
+  {
+    "revision": "9eed903a2e0b97d43a56",
+    "url": "/static/js/runtime~main.9eed903a.js"
+  },
+  {
+    "revision": "f54fb85e3cd9e5e9c30f238045632816",
+    "url": "/index.html"
+  }
+];
